67d6735328670b968093836e5719847d9b088568
Bobby Wang <bobby.wang25@gmail.com>
2021-01-06 12:59:07 -0500

    second activity finished

--- app/src/main/java/com/palepeak/introapplication/MainActivity.kt
+++ app/src/main/java/com/palepeak/introapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.palepeak.introapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("MESSAGE", "TEST MESSAGE")
+            startActivity(intent)
+        }
     }
 }
